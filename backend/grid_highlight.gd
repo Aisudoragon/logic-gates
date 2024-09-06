e4ea6bf49fbbb80d34e2905be7c6ebe3f4deff90
Aisudoragon <krystiandziura101@gmail.com>
2024-09-06 15:49:16 +0200

    Wire placement shape

--- backend/grid_highlight.gd
+++ backend/grid_highlight.gd
@@ -1,6 +1,36 @@
 extends TileMapLayer
 
+var start_drawing_position : Vector2i
+var pressed_before := false
 
-func _unhandled_input(_event: InputEvent) -> void:
+
+func _unhandled_input(event: InputEvent) -> void:
 	clear()
-	set_cell(local_to_map(get_local_mouse_position()), 1, Vector2i(0,0), 0)
+	var mouse_position: Vector2i = local_to_map(get_local_mouse_position())
+	set_cell(mouse_position, 0, Vector2i(0,0), 1)
+
+	if event.is_action_pressed("place"):
+		start_drawing_position = mouse_position
+		pressed_before = true
+	if event.is_action_released("place"):
+		pressed_before = false
+
+	if pressed_before:
+		var distance_2d : Vector2i = mouse_position - start_drawing_position
+		var lenghter : int = maxi(absi(distance_2d.x), absi(distance_2d.y))
+
+		for i : int in range(0, absi(lenghter)):
+			var direction : Vector2i = distance_2d.sign()
+			var new_cell_pos := Vector2i(
+					clampi(
+							start_drawing_position.x + (i * direction.x),
+							mini(start_drawing_position.x, mouse_position.x),
+							maxi(start_drawing_position.x, mouse_position.x)
+					),
+					clampi(
+							start_drawing_position.y + (i * direction.y),
+							mini(start_drawing_position.y, mouse_position.y),
+							maxi(start_drawing_position.y, mouse_position.y)
+					)
+			)
+			set_cell(new_cell_pos, 0, Vector2i(0,0), 1)
--- backend/wire_grid_manipulation.gd
+++ backend/wire_grid_manipulation.gd
@@ -1,30 +1,30 @@
 extends TileMapLayer
 
+var start_drawing_position : Vector2i
+
 
 func _unhandled_input(event: InputEvent) -> void:
 	var mouse_position: Vector2i = local_to_map(get_local_mouse_position())
-	
+
 	if event.is_action_pressed("place"):
-		set_cell(mouse_position, 1, Vector2i(0,0), 0)
-		update_wire_look(mouse_position, true)
-	
+		start_drawing_position = mouse_position
+	if event.is_action_released("place"):
+		var distance_2d : Vector2i = mouse_position - start_drawing_position
+		var lenghter : int = maxi(absi(distance_2d.x), absi(distance_2d.y))
+
+		for i : int in range(0, absi(lenghter) + 1):
+			var direction : Vector2i = distance_2d.sign()
+			var new_cell_pos := Vector2i(
+					clampi(start_drawing_position.x + (i * direction.x),
+							mini(start_drawing_position.x, mouse_position.x),
+							maxi(start_drawing_position.x, mouse_position.x)
+					),
+					clampi(start_drawing_position.y + (i * direction.y),
+							mini(start_drawing_position.y, mouse_position.y),
+							maxi(start_drawing_position.y, mouse_position.y)
+					)
+			)
+			set_cell(new_cell_pos, 0, Vector2i(0,0), 0)
+
 	if event.is_action_pressed("destroy"):
 		erase_cell(mouse_position)
-		update_wire_look(mouse_position, false)
-
-
-func update_wire_look(mouse_position: Vector2i, created: bool) -> void:
-	var neighbors: Array[Vector2i] = get_propoer_neighbors(mouse_position)
-
-
-func get_propoer_neighbors(center: Vector2i) -> Array[Vector2i]:
-	return [
-		get_neighbor_cell(center, TileSet.CELL_NEIGHBOR_RIGHT_SIDE),
-		get_neighbor_cell(center, TileSet.CELL_NEIGHBOR_BOTTOM_RIGHT_CORNER),
-		get_neighbor_cell(center, TileSet.CELL_NEIGHBOR_BOTTOM_SIDE),
-		get_neighbor_cell(center, TileSet.CELL_NEIGHBOR_BOTTOM_LEFT_CORNER),
-		get_neighbor_cell(center, TileSet.CELL_NEIGHBOR_LEFT_SIDE),
-		get_neighbor_cell(center, TileSet.CELL_NEIGHBOR_TOP_LEFT_CORNER),
-		get_neighbor_cell(center, TileSet.CELL_NEIGHBOR_TOP_SIDE),
-		get_neighbor_cell(center, TileSet.CELL_NEIGHBOR_TOP_RIGHT_CORNER),
-	]

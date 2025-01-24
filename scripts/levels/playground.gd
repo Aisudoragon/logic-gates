26ff6c3fc0bf374de8c636e9a08c3b67143dd54b
Aisudoragon <krystiandziura101@gmail.com>
2025-01-24 18:20:12 +0100

    Wire properly applies to it's layer

--- scripts/levels/playground.gd
+++ scripts/levels/playground.gd
@@ -5,7 +5,6 @@ var previous_tile: Vector2i
 @onready var gate_layer: TileMapLayer = $GateLayer
 @onready var highlight_layer: TileMapLayer = $HighlightLayer
 @onready var logic_layer: TileMapLayer = $LogicLayer
-@onready var camera: Camera2D = $Camera2D
 
 
 func _unhandled_input(event: InputEvent) -> void:
@@ -15,18 +14,21 @@ func _unhandled_input(event: InputEvent) -> void:
 		var event_mb: InputEventMouseButton = event
 		if event.is_pressed() and event_mb.button_index == MOUSE_BUTTON_LEFT:
 			previous_tile = mouse_position
+		if event.is_released() and event_mb.button_index == MOUSE_BUTTON_LEFT:
+			copy_highlight_into_layer()
 
 	if event is InputEventMouseMotion:
 		if Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT) and not mouse_position == previous_tile:
-			connect_wire(mouse_position)
+			var direction: Vector2i = mouse_position - previous_tile
+			update_wire_look(previous_tile, direction)
+			direction *= -1
+			update_wire_look(mouse_position, direction)
+
 			previous_tile = mouse_position
+		if not Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT):
+			highlight_layer.clear()
+			update_wire_look(mouse_position, Vector2i.ZERO)
 
-
-func connect_wire(next_tile: Vector2i) -> void:
-	var direction: Vector2i = next_tile - previous_tile
-	print(direction)
-	update_wire_look(previous_tile, direction)
-	update_wire_look(next_tile, direction)
 
 
 func update_wire_look(tile_position: Vector2i, new_direction: Vector2i) -> void:
@@ -37,10 +39,24 @@ func update_wire_look(tile_position: Vector2i, new_direction: Vector2i) -> void:
 
 	var data: TileData = highlight_layer.get_cell_tile_data(tile_position)
 	if data:
-		right_direction = data.get_custom_data("right_direction")
-		bottom_direction = data.get_custom_data("bottom_direction")
-		left_direction = data.get_custom_data("left_direction")
-		up_direction = data.get_custom_data("up_direction")
+		if data.get_custom_data("right_direction"):
+			right_direction = true
+		if data.get_custom_data("bottom_direction"):
+			bottom_direction = true
+		if data.get_custom_data("left_direction"):
+			left_direction = true
+		if data.get_custom_data("up_direction"):
+			up_direction = true
+	data = wire_layer.get_cell_tile_data(tile_position)
+	if data:
+		if data.get_custom_data("right_direction"):
+			right_direction = true
+		if data.get_custom_data("bottom_direction"):
+			bottom_direction = true
+		if data.get_custom_data("left_direction"):
+			left_direction = true
+		if data.get_custom_data("up_direction"):
+			up_direction = true
 
 	if new_direction == Vector2i.RIGHT:
 		right_direction = true
@@ -111,3 +127,14 @@ func update_wire_look(tile_position: Vector2i, new_direction: Vector2i) -> void:
 		# top
 		highlight_layer.set_cell(tile_position, 0, Vector2i(0, 0), 2)
 		return
+	highlight_layer.set_cell(tile_position, 0, Vector2i(5, 0), 0)
+
+
+func copy_highlight_into_layer() -> void:
+	var all_tiles: Array[Vector2i] = highlight_layer.get_used_cells()
+
+	for coordinates: Vector2i in all_tiles:
+		var atlas_coords: Vector2i = highlight_layer.get_cell_atlas_coords(coordinates)
+		var alternative: int = highlight_layer.get_cell_alternative_tile(coordinates)
+
+		wire_layer.set_cell(coordinates, 0, atlas_coords, alternative)

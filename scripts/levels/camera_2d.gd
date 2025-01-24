7c8f1496872987939bd3acefc4a7b1355115ae94
Aisudoragon <krystiandziura101@gmail.com>
2025-01-24 17:22:36 +0100

    Fixed warnings

--- scripts/levels/camera_2d.gd
+++ scripts/levels/camera_2d.gd
@@ -1,17 +1,19 @@
 extends Camera2D
 
-
 var zoomSpeed: float = 0.05
 var zoomMin: float = 0.05
 var zoomMax: float = 2.0
 var dragSensitivity: float = 1.0
 
+
 func _input(event: InputEvent) -> void:
-	if event is InputEventMouseMotion and Input.is_mouse_button_pressed(MOUSE_BUTTON_MIDDLE):
-		position -= event.relative * dragSensitivity / zoom
 	if event is InputEventMouseButton:
-		if event.button_index == MOUSE_BUTTON_WHEEL_UP:
+		var event_mb: InputEventMouseButton = event
+		if event_mb.button_index == MOUSE_BUTTON_WHEEL_UP:
 			zoom += Vector2(zoomSpeed, zoomSpeed)
-		elif event.button_index == MOUSE_BUTTON_WHEEL_DOWN:
+		elif event_mb.button_index == MOUSE_BUTTON_WHEEL_DOWN:
 			zoom -= Vector2(zoomSpeed, zoomSpeed)
 		zoom = clamp(zoom, Vector2(zoomMin, zoomMin), Vector2(zoomMax, zoomMax))
+	if event is InputEventMouseMotion and Input.is_mouse_button_pressed(MOUSE_BUTTON_MIDDLE):
+		var event_mm: InputEventMouseMotion = event
+		position -= event_mm.relative * dragSensitivity / zoom

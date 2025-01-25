7e7ef2de1aa65f8fd324146e676110e33288805e
Aisudoragon <krystiandziura101@gmail.com>
2025-01-25 02:27:21 +0100

    Added very simple concept for spreading logic

--- scripts/levels/playground.gd
+++ scripts/levels/playground.gd
@@ -11,7 +11,7 @@ func _unhandled_input(event: InputEvent) -> void:
 	var mouse_position: Vector2i = wire_layer.local_to_map(wire_layer.get_local_mouse_position())
 
 	if event is InputEventMouseButton:
-		var event_mb: InputEventMouseButton = event
+		var event_mb := event as InputEventMouseButton
 		if event.is_pressed() and event_mb.button_index == MOUSE_BUTTON_LEFT:
 			previous_tile = mouse_position
 		if event.is_released() and event_mb.button_index == MOUSE_BUTTON_LEFT:
@@ -29,6 +29,14 @@ func _unhandled_input(event: InputEvent) -> void:
 			highlight_layer.clear()
 			update_wire_look(mouse_position, Vector2i.ZERO)
 
+	if Input.is_key_pressed(KEY_1):
+		gate_layer.set_cell(mouse_position, 0, Vector2i(0, 0))
+		logic_layer.set_cell(mouse_position, 0, Vector2i(0, 0))
+	if Input.is_key_pressed(KEY_2):
+		gate_layer.set_cell(mouse_position, 0, Vector2i(1, 0), 2)
+		logic_layer.set_cell(mouse_position, 0, Vector2i(0, 0))
+	if Input.is_key_pressed(KEY_ENTER):
+		toogle_start_gate()
 
 
 func update_wire_look(tile_position: Vector2i, new_direction: Vector2i) -> void:
@@ -133,8 +141,28 @@ func update_wire_look(tile_position: Vector2i, new_direction: Vector2i) -> void:
 func copy_highlight_into_layer() -> void:
 	var all_tiles: Array[Vector2i] = highlight_layer.get_used_cells()
 
-	for coordinates: Vector2i in all_tiles:
+	for coordinates in all_tiles:
 		var atlas_coords: Vector2i = highlight_layer.get_cell_atlas_coords(coordinates)
 		var alternative: int = highlight_layer.get_cell_alternative_tile(coordinates)
 
 		wire_layer.set_cell(coordinates, 0, atlas_coords, alternative)
+		if logic_layer.get_cell_source_id(coordinates) == -1:
+			logic_layer.set_cell(coordinates, 0, Vector2i(0, 0))
+
+
+func toogle_start_gate() -> void:
+	var gates: Array[Vector2i] = gate_layer.get_used_cells_by_id(0, Vector2i(0, 0))
+	for gate_pos in gates:
+		var state: int = logic_layer.get_cell_alternative_tile(gate_pos)
+		if state == 0:
+			logic_layer.set_cell(gate_pos, 0, Vector2i(0, 0), 1)
+		else:
+			logic_layer.set_cell(gate_pos, 0, Vector2i(0, 0), 0)
+		update_state(gate_pos, state)
+
+
+func update_state(tile_coords: Vector2i, state: bool) -> void:
+	logic_layer.set_cell(tile_coords, 0, Vector2i(0, 0), not state)
+	var wire_data: TileData = wire_layer.get_cell_tile_data(tile_coords)
+	if wire_data.get_custom_data("right_direction"):
+		update_state(tile_coords + Vector2i.RIGHT, state)
